f8c334546e4fce836bf07aabb7686099b53eed4c
Jane Sullivan <jane.sullivan@noaa.gov>
2020-10-16 09:28:47 -0800

    commit indicator development to date

---
+++ R/0_queries.R
@@ -0,0 +1,99 @@
+# Queries for sablefish condition factor
+# Contact: jane.sullivan@noaa.gov
+# Last updated: Oct 2020
+
+# devtools::session_info()
+# version  R version 4.0.2 (2020-06-22)
+# os       Windows 10 x64              
+# system   x86_64, mingw32             
+# ui       RStudio 
+
+# Set up ----
+
+# Most recent survey year 
+YEAR <- 2020
+
+# Create a year subdirectory to store annual data used for condition factors
+dat_path <- paste0("data/", YEAR)
+dir.create(dat_path)
+raw_path <- paste0(dat_path, "/raw") # raw data
+dir.create(raw_path) 
+
+libs <- c("tidyverse", "RODBC")
+if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {
+  install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
+lapply(libs, library, character.only = TRUE)
+
+# connect to longline database
+conn <- odbcDriverConnect(connection="Driver={SQL Server};server=161.55.120.51,1919;database=LONGLINE;trusted_connection=yes;")
+
+# afsc/akfin dbs
+db <- read_csv("database.csv") # user-specific usernames and passwords, not tracked on github
+
+# database_afsc <- "afsc"
+# username_afsc <- db$username_afsc 
+# password_afsc <- db$password_afsc 
+# channel_afsc <- odbcConnect(database_afsc, uid = username_afsc, pwd = password_afsc, believeNRows=FALSE)
+
+database_akfin <- "akfin" 
+username_akfin <- db$username_akfin
+password_akfin <- db$password_akfin
+channel_akfin <- odbcConnect(database_akfin, uid = username_akfin, pwd = password_akfin, believeNRows=FALSE)
+
+# Sablefish survey specimen data -----
+query <- "select  *
+          from    Age_View"
+
+sable <- sqlQuery(conn, query)
+write_csv(sable, paste0(raw_path, "/sable_bio_", min(sable$Year), "_", YEAR, ".csv"))
+
+sable <- sable %>% rename_all(tolower) 
+
+# Create some reference tables
+(sable %>% distinct(maturity, maturitydescription) %>% write_csv("data/maturity_codes.csv"))
+(sable %>% distinct(sex, sexdescription) %>% write_csv("data/sex_codes.csv"))
+(sable %>% distinct(stratum, stratumdescription) %>% write_csv("data/stratum_decriptions.csv"))
+(sable %>% distinct(error_flag, errordescription) %>% write_csv("data/error_codes.csv"))
+
+sable %>% 
+  # select(-c(maturitydescription, sexdescription, stratumdescription, errordescription)) %>% 
+  write_csv(paste0(dat_path, "/sable_bio_", min(sable$year), "_", YEAR, ".csv"))
+
+# Survey stations -----
+
+query <- "select  *
+          from Stations_View_AKFIN"
+
+
+stations <- sqlQuery(conn, query)
+write_csv(stations, paste0(raw_path, "/sable_survey_stations.csv"))
+
+stations <- stations %>% rename_all(tolower) 
+
+stations %>% write_csv(paste0(dat_path, "/sable_survey_stations.csv"))
+
+# Observer/fishery specimen data ----
+
+query <- paste0("select   year, nmfs_area, gear, species,
+                          length, weight, age, maturity_code, maturity_description,
+                          sex, bottom_depth_fathoms, haul_offload_date,
+                          sample_system, specimen_type, specimen_type_description,
+                          performance
+                 from     norpac.debriefed_age
+                 where    species in '203'") 
+
+fsh <- sqlQuery(channel_akfin, query) %>% 
+  write_csv(paste0(raw_path, "/sable_fishery_bio_", YEAR, ".csv"))
+
+# NMFS area look up 
+query <- "select  distinct  fmp_area as fmp, fmp_subarea, 
+                            reporting_area_code as nmfs_area
+          from              council.comprehensive_blend_ca"
+
+nmfs_area_lookup <- sqlQuery(channel_akfin, query) %>% rename_all(tolower)
+
+fsh %>% 
+  rename_all(tolower) %>%
+  mutate(nmfs_area = as.character(nmfs_area)) %>% 
+  left_join(nmfs_area_lookup)  %>% 
+  write_csv(paste0(dat_path, "/sable_fishery_bio_", YEAR, ".csv"))
---
+++ R/1_condition.R
@@ -0,0 +1,490 @@
+# Sablefish condition indicator analysis
+# Contact: jane.sullivan@noaa.gov
+# Last updated: Oct 2020
+
+# devtools::session_info()
+# version  R version 4.0.2 (2020-06-22)
+# os       Windows 10 x64              
+# system   x86_64, mingw32             
+# ui       RStudio 
+
+# Set up ----
+
+# Most recent survey year 
+YEAR <- 2020
+
+dat_path <- paste0("data/", YEAR) # directory where source data is contained
+out_path <- paste0("results/", YEAR) # directory for results/output
+dir.create(out_path)
+
+libs <- c("knitr", "tidyverse", "devtools", "viridis")
+if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {
+  install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
+lapply(libs, library, character.only = TRUE)
+
+devtools::install_github("sean-rohan-NOAA/akfishcondition", dep = FALSE)
+library(akfishcondition)
+pkg_version <- packageVersion("akfishcondition")
+
+# Survey data ----
+
+sable <- read_csv(paste0(dat_path, "/sable_bio_1981_", YEAR, ".csv"),
+                  guess_max = 1e6)
+
+# Create some reference tables
+sable %>% distinct(maturity, maturitydescription) 
+sable %>% distinct(sex, sexdescription) 
+sable %>% distinct(stratum, stratumdescription) 
+sable %>% distinct(error_flag, errordescription) 
+
+# General clean up
+dim(sable)
+sable <- sable %>% filter(!is.na(length) & !is.na(weight) & !is.na(maturity))
+dim(sable)
+sable <- sable %>% filter(error_flag == 0) # incomplete or erroneous specimen dat
+dim(sable)
+sable <- sable %>% filter(station_number <= 499) # get rid of experimental stations
+dim(sable)
+sable <- sable %>% filter(!council_sablefish_management_area %in% # just keep legs 3-7 = GOA (omit BSAI), per KS guidance 10/14/20
+                            c("Aleutians", "Bering Sea"))
+dim(sable)
+
+# defined mature = 1, immature = 0
+sable <- sable %>% mutate(mature = ifelse(maturity %in% c(1, 2), 0, 1))
+
+# Survey station data ----
+stations <- read_csv(paste0(dat_path, "/sable_survey_stations.csv"),
+                     guess_max = 1e6)
+dim(stations)
+stations %>% dplyr::count(habitat_type)
+
+stations <- stations %>% 
+  mutate(habitat = case_when(habitat_type == "Deep cross-shelf gully" ~ "deep_gully",
+                             habitat_type == "Shallow cross-shelf gully" ~ "shallow_gully",
+                             habitat_type == "Upper continental slope" ~ "slope"))
+
+# habitat and other station info
+sable <- sable %>% left_join(stations %>% select(station_number, habitat, active))
+sable %>% dplyr::count(active) # check that all stations are active
+
+# KS suggested stations should be cut off >=76. Not sure about this. Mine start
+# at 62 and I've removed experimental stations. The stations between 62 and 76
+# are active stations in slope habitat in the WGOA and CGOA. Can't see a reason
+# to omit them.
+range(sable$station_number)
+stations %>% 
+  filter(between(station_number, 62, 76)) %>%
+  distinct(active, rpw_flag, nmfs_area_code, council_sablefish_management_area, habitat)
+
+# Fishery data ----
+fsh <- read_csv(paste0(dat_path, "/sable_fishery_bio_", YEAR, ".csv"),
+                guess_max = 1e6)
+
+fsh %>% dplyr::count(maturity_code, maturity_description) # VERY little maturity data, ignore it!
+dim(fsh)  
+fsh <- fsh %>% filter(!is.na(length) & !is.na(weight) & sex != "U")
+dim(fsh)
+
+# Use consistent sex codes to survey
+fsh <- fsh %>% mutate(sex = ifelse(sex == "F", 2, 1))
+
+# Keep BSAI and GOA
+fsh <- fsh %>% filter(fmp %in% c("BSAI", "GOA"))
+
+fsh %>% dplyr::count(year, sex, fmp) %>% 
+  filter(sex == 2) %>% 
+  arrange(fmp) %>% 
+  print(n = Inf)
+
+# Sampling really picked up 1999. Also cut off current year
+fsh <- fsh %>% filter(between(year, 1999, YEAR-1))
+
+# EDA - delete later ----
+
+# concern there will be too many mature fish in this group
+sable %>% 
+  filter(sex == 2 & length <= 55) %>% 
+  group_by(year) %>% 
+  dplyr::summarize(n()) %>% 
+  print(n = Inf)
+
+sable %>% 
+  filter(sex == 2 & age == 4) %>% 
+  group_by(year) %>% 
+  dplyr::summarize(n()) %>% 
+  print(n = Inf)
+
+sable %>% 
+  filter(sex == 2 & age == 4) %>% 
+  group_by(year, mature) %>% 
+  dplyr::summarize(n()) %>% 
+  pivot_wider(id_cols = year, names_from = mature, values_from = `n()`) %>% 
+  print(n = Inf)
+
+# Potential indicator groups ----
+
+# 1: GOA Survey Immature age-4 females ----
+
+sable1 <- sable %>% 
+  filter(sex == 2 & age == 4 & mature == 0) %>% 
+  mutate(condition_stratum = NA,
+         species_code = 205101, # create unique specimen name for each grp
+         common_name = "GOA LL Survey: Age-4 immature females",
+         # added area biomass = 1 (equal weighting)
+         area_biomass = 1)
+sable1 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# sable1 %>% 
+#   ggplot(aes(x = log(length), y = log(weight), col = factor(year))) +
+#   geom_point() +
+#   geom_smooth(method = 'lm')
+
+# We can't reach sample size thresholds of 10 if we split by stratum
+# sable1 %>% 
+#   dplyr::count(year, stratum) %>% 
+#   arrange(stratum) %>% 
+#   pivot_wider(id_cols = year, names_from = stratum, values_from = n) %>% 
+#   arrange(year) %>% 
+#   print(n = Inf)
+
+# Create new "shallow" vs "deep" strata cutoff at 300 m - still can't meet sample sizes
+# sable1 <- sable1 %>% 
+#   mutate(condition_stratum = ifelse(stratum %in% c(1, 2, 3), "shallow", "deep"))
+
+# sable1 %>% 
+#   dplyr::count(year, condition_stratum) %>% 
+#   pivot_wider(id_cols = year, names_from = condition_stratum, values_from = n) %>% 
+#   arrange(year) %>% 
+#   print(n = Inf)
+
+# 2: GOA Survey All age-4 females ----
+
+sable2 <- sable %>% 
+  filter(sex == 2 & age == 4) %>% 
+  mutate(condition_stratum = NA,
+         species_code = 205102,
+         common_name = "GOA LL Survey: Age-4 immature + mature females",
+         area_biomass = 1) 
+sable2 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# 3: GOA Survey Females >= 75 cm ----
+sable3 <- sable %>% 
+  filter(sex == 2 & length >= 75) %>% 
+  mutate(condition_stratum = NA,
+         species_code = 205103,
+         common_name = "GOA LL Survey: Large females >= 75 cm",
+         area_biomass = 1)
+
+sable3 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# 4: GOA Survey Immature females <= 55 cm ----
+sable4 <- sable %>% 
+  filter(sex == 2 & length <= 55 & mature == 0) %>%
+  mutate(condition_stratum = NA,
+         species_code = 205104,
+         common_name = "GOA LL Survey: Immature juvenile females <= 55 cm",
+         # added area biomass = 1 (equal weighting)
+         area_biomass = 1)
+
+sable4 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# 5: GOA Fishery: large females >= 75 cm ----
+sable5 <- fsh %>% 
+  filter(sex == 2 & length >= 75 & fmp == "GOA") %>%
+  mutate(condition_stratum = NA,
+         species_code = 205105,
+         common_name = "GOA LL Fishery: Large females >= 75 cm",
+         area_biomass = 1)
+sable5 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# 6: BSAI Fishery: large females >= 75 cm ----
+sable6 <- fsh %>% 
+  filter(sex == 2 & length >= 75 & fmp == "BSAI") %>%
+  mutate(condition_stratum = NA,
+         species_code = 205106,
+         common_name = "BSAI LL Fishery: Large females >= 75 cm",
+         area_biomass = 1)
+sable6 %>% 
+  dplyr::count(year) %>% 
+  print(n = Inf)
+
+# Prep data for condition analysis ----
+sable_sub <- sable1 %>% 
+  bind_rows(sable2) %>% 
+  bind_rows(sable3) %>% 
+  bind_rows(sable4) %>% 
+  bind_rows(sable5) %>% 
+  bind_rows(sable6) %>% 
+  select(common_name, species_code, year, 
+         condition_stratum, area_biomass, length, weight)
+
+(sable_options <- unique(sable_sub$species_code))
+
+# Calculate length weight residuals -----
+for(i in 1:length(sable_options)) {
+  
+  # Separate slope for each stratum. Bias correction according to Brodziak, no outlier detection.
+  sable_df <- calc_lw_residuals(len = sable_sub$length[sable_sub$species_code == sable_options[i]], 
+                                wt = sable_sub$weight[sable_sub$species_code == sable_options[i]], 
+                                year = sable_sub$year[sable_sub$species_code == sable_options[i]], 
+                                stratum = sable_sub$condition_stratum[sable_sub$species_code == sable_options[i]], 
+                                make_diagnostics = TRUE, # Make diagnostics
+                                bias.correction = TRUE, # Bias correction turned on
+                                outlier.rm = FALSE, # Outlier removal turned on
+                                region = "sable_indicator_group", #"AI",
+                                species_code = sable_sub$species_code[sable_sub$species_code == sable_options[i]],
+                                include_ci = TRUE)
+  sable_sub$resid_mean[sable_sub$species_code == sable_options[i]] <- sable_df$lw.res_mean
+  sable_sub$resid_lwr[sable_sub$species_code == sable_options[i]] <- sable_df$lw.res_lwr
+  sable_sub$resid_upr[sable_sub$species_code == sable_options[i]] <- sable_df$lw.res_upr
+  
+}
+
+# Estimate mean and std. err for each stratum, filter out strata with less than 10 samples
+sable_resids <- sable_sub %>% 
+  dplyr::group_by(common_name, species_code, year, condition_stratum, area_biomass) %>% 
+  # dplyr::group_by(COMMON_NAME, SPECIES_CODE, YEAR, INPFC_STRATUM, AREA_BIOMASS) %>%
+  dplyr::summarise(stratum_resid_mean = mean(resid_mean),
+                   stratum_resid_sd = sd(resid_mean),
+                   n = n()) %>%
+  dplyr::filter(n >= 10) %>%
+  dplyr::mutate(stratum_resid_se = stratum_resid_sd/sqrt(n))
+
+# Weight strata by biomass 
+for(i in 1:length(sable_options)) {
+  
+  sable_resids$weighted_resid_mean[sable_resids$species_code == sable_options[i]] <- weight_lw_residuals(
+    residuals = sable_resids$stratum_resid_mean[sable_resids$species_code == sable_options[i]],
+    year = sable_resids$year[sable_resids$species_code == sable_options[i]],
+    # stratum = rep("no_strata", nrow(sable_resids)),
+    stratum = sable_resids$condition_stratum[sable_resids$species_code == sable_options[i]],
+    stratum_biomass = sable_resids$area_biomass[sable_resids$species_code == sable_options[i]])
+  
+  sable_resids$weighted_resid_se[sable_resids$species_code == sable_options[i]] <- weight_lw_residuals(
+    residuals = sable_resids$stratum_resid_se[sable_resids$species_code == sable_options[i]],
+    year = sable_resids$year[sable_resids$species_code == sable_options[i]],
+    # stratum = rep("no_strata", nrow(sable_resids)),
+    stratum = sable_resids$condition_stratum[sable_resids$species_code == sable_options[i]],
+    stratum_biomass = sable_resids$area_biomass[sable_resids$species_code == sable_options[i]])
+}
+
+# Biomass-weighted residual and SE by year
+sable_ann_mean_resid_df <- sable_resids %>% 
+  dplyr::group_by(year, common_name) %>%
+  dplyr::summarise(mean_wt_resid = mean(weighted_resid_mean),
+                   se_wt_resid = mean(weighted_resid_se))
+
+# Plot adults -----
+
+sable_ann_mean_resid_df %>% 
+  filter(grepl("Large", common_name)) %>% 
+  ggplot() + 
+  geom_bar(aes(x = year, 
+               y = mean_wt_resid), 
+           stat = "identity", 
+           fill = "goldenrod", 
+           color = "black") +
+  geom_errorbar(aes(x = year, 
+                    ymax = mean_wt_resid + 1.96 * se_wt_resid,
+                    ymin = mean_wt_resid - 1.96 * se_wt_resid),
+                width = 0.2) +
+  geom_hline(yintercept = 0) +
+  # geom_point(data = old_indicator_dat, 
+  #            aes(x = YEAR, 
+  #                y = ymeans)) +
+  facet_wrap(~common_name, ncol = 1) + #, scales = "free_y"
+  scale_x_continuous(name = "Year") +
+  scale_y_continuous(name = "Length-weight residual (ln(g))") +
+  theme_minimal() +
+  ggtitle("Condition indicators for large female sablefish\n")
+
+ggsave(paste0(out_path, "/large_sable_condition_", YEAR, ".png"))
+
+# Plot smaller fish ----
+
+sable_ann_mean_resid_df %>% 
+  filter(grepl("Age-4|Immature|immature|age-4", common_name)) %>% 
+  ggplot() + 
+  geom_bar(aes(x = year, 
+               y = mean_wt_resid), 
+           stat = "identity", 
+           fill = "plum", 
+           color = "black") +
+  geom_errorbar(aes(x = year, 
+                    ymax = mean_wt_resid + 1.96 * se_wt_resid,
+                    ymin = mean_wt_resid - 1.96 * se_wt_resid),
+                width = 0.2) +
+  geom_hline(yintercept = 0) +
+  # geom_point(data = old_indicator_dat, 
+  #            aes(x = YEAR, 
+  #                y = ymeans)) +
+  facet_wrap(~common_name, ncol = 1) + #, scales = "free_y"
+  scale_x_continuous(name = "Year") +
+  scale_y_continuous(name = "Length-weight residual (ln(g))") +
+  theme_minimal() +
+  ggtitle("Condition indicators for young/immature female sablefish\n")
+
+ggsave(paste0(out_path, "/small_sable_condition_", YEAR, ".png"))
+
+# 
+# # Restandardize ----
+# sable_ann_mean_resid_df %>% 
+#   filter(common_name == "Sablefish: age-4 immature females") %>% 
+#   select(year, mean_wt_resid) %>% 
+#   mutate(rescale = scales::rescale(mean_wt_resid, -1, 1))
+
+# Write results ----
+
+out_file <- sable_sub %>% 
+  group_by(common_name, year) %>% 
+  dplyr::summarize(n = n()) %>% 
+  left_join(sable_ann_mean_resid_df) %>% 
+  select(indicator = common_name, year, n, mean_wt_resid, se_wt_resid) %>% 
+  dplyr::arrange(indicator)
+write_csv(out_file, paste0(out_path, "/sable_condition_indicators_", YEAR, ".csv"))
+
+# move diagnostics ----
+
+# clean up the default printing of diagnostic files for consistency with this
+# project - move diagnostic output, delete default folder
+diag_out <- file.path(paste0(out_path, "/diagnostics"))
+dir.create(diag_out)
+
+default_diag <- file.path("output/sable_indicator_group")
+diag_ls <- list.files(default_diag)
+diag_ls <- paste0(default_diag, "/", diag_ls)
+file.copy(from = diag_ls, to = diag_out, overwrite = TRUE, recursive = FALSE, 
+          copy.mode = TRUE)
+
+unlink(file.path("output"), recursive = TRUE)
+
+# weight-at-age plots ----
+sable %>% 
+  select(year, sex, age, weight) %>% 
+  filter(!is.na(age) & sex %in% c(1, 2)) %>%
+  group_by(year, sex, age) %>% 
+  dplyr::summarise(weight = mean(weight) %>% round(4)) %>%  
+  ungroup() %>% 
+  mutate(Year = as.character(year),
+         Sex = factor(sex, levels = c(1, 2), labels = c("Male", "Female")),
+         Age = factor(age),
+         cohort = year - age,
+         Cohort = as.factor(cohort))-> df
+
+library(ggthemes)
+pal <- ggthemes::canva_pal("Warm and cool")(4) 
+
+YEAR = 2019
+# By cohort
+df_cohort <- df %>% 
+  filter(cohort >= 2000 & cohort <= YEAR-3 & age >=2 & age <= 5) %>% 
+  droplevels()
+
+# Axis ticks for plot (see helper.r tickr() fxn for details)
+# axis <- tickr(df_cohort, year, 2)
+
+ggplot(df_cohort, aes(year, weight, colour = Cohort, group = Cohort)) +
+  geom_line(size = 1) +
+  geom_point(aes(fill = Cohort), show.legend = FALSE, size = 1) +
+  facet_grid(~Sex) +
+  labs(x = "Year", y = "Weight-at-age (kg)\n", colour = "Cohort") +
+  guides(colour = guide_legend(ncol = 9)) +
+  scale_colour_manual(values = colorRampPalette(pal)(n_distinct(df_cohort$Cohort))) +
+  theme(legend.position = "bottom") #+
+  # scale_x_continuous(breaks = axis$breaks, labels = axis$labels)# -> waa_cohort_plot
+
+# ggsave("figures/waa_cohort.png", dpi = 300, height = 5, width = 7, units = "in")
+
+df %>% 
+  filter(Age %in% c("4", "5", "6")) %>% 
+  droplevels() -> df
+
+df %>% 
+  group_by(Age, Sex) %>% 
+  dplyr::summarize(mean_weight = mean(weight, na.rm = TRUE)) -> means
+
+# axis <- tickr(df, year, 5)
+
+ggplot(df, 
+       aes(year, weight, group = Age, colour = Age)) + 
+  geom_line() + 
+  geom_point() +
+  facet_wrap(~ Sex, ncol = 1, scales = "free") +
+  geom_hline(data = means, aes(colour = Age, yintercept = mean_weight), alpha = 0.4, linetype = 2) + 
+  labs(x = "Year", y = "Weight-at-age (kg)\n", colour = "Age") +
+  scale_colour_manual(values = colorRampPalette(pal)(n_distinct(df$Age))) +
+  guides(colour = guide_legend(nrow = 1)) +
+  theme_minimal() +
+  theme(legend.position = "bottom")#+
+  # scale_x_continuous(breaks = axis$breaks, labels = axis$labels)
+
+ggsave(paste0(out_path, "/sable_srv_waa.png"))
+
+# length-at-age ----
+sable %>% 
+  select(year, sex, age, length) %>% 
+  filter(!is.na(age) & sex %in% c(1, 2)) %>%
+  group_by(year, sex, age) %>% 
+  dplyr::summarise(length = mean(length) %>% round(4)) %>%  
+  ungroup() %>% 
+  mutate(Year = as.character(year),
+         Sex = factor(sex, levels = c(1, 2), labels = c("Male", "Female")),
+         Age = factor(age),
+         cohort = year - age,
+         Cohort = as.factor(cohort))-> df
+
+library(ggthemes)
+pal <- ggthemes::canva_pal("Warm and cool")(4) 
+
+YEAR = 2019
+# By cohort
+df_cohort <- df %>% 
+  filter(cohort >= 2000 & cohort <= YEAR-3 & age >=2 & age <= 5) %>% 
+  droplevels()
+
+# Axis ticks for plot (see helper.r tickr() fxn for details)
+# axis <- tickr(df_cohort, year, 2)
+
+ggplot(df_cohort, aes(year, length, colour = Cohort, group = Cohort)) +
+  geom_line(size = 1) +
+  geom_point(aes(fill = Cohort), show.legend = FALSE, size = 1) +
+  facet_grid(~Sex) +
+  labs(x = "Year", y = "length-at-age (cm)\n", colour = "Cohort") +
+  guides(colour = guide_legend(ncol = 9)) +
+  scale_colour_manual(values = colorRampPalette(pal)(n_distinct(df_cohort$Cohort))) +
+  theme(legend.position = "bottom") #+
+# scale_x_continuous(breaks = axis$breaks, labels = axis$labels)# -> waa_cohort_plot
+
+df %>% 
+  filter(Age %in% c("4", "5", "6")) %>% 
+  droplevels() -> df
+df %>% 
+  group_by(Age, Sex) %>% 
+  dplyr::summarize(mean_length = mean(length, na.rm = TRUE)) -> means
+
+# axis <- tickr(df, year, 5)
+
+ggplot(df, 
+       aes(year, length, group = Age, colour = Age)) + 
+  geom_line() + 
+  geom_point() +
+  facet_wrap(~ Sex, ncol = 1, scales = "free") +
+  # geom_hline(data = means, aes(colour = Age, yintercept = mean_length), alpha = 0.4, linetype = 2) + 
+  labs(x = "Year", y = "Length-at-age (cm)\n", colour = "Age") +
+  scale_colour_manual(values = colorRampPalette(pal)(n_distinct(df$Age))) +
+  guides(colour = guide_legend(nrow = 1)) +
+  theme_minimal() +
+  theme(legend.position = "bottom")#+
+# scale_x_continuous(breaks = axis$breaks, labels = axis$labels)
+
+ggsave(paste0(out_path, "/sable_srv_laa.png"))
